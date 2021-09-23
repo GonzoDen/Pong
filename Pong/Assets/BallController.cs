8db8b2ffdbf8e00239ffcae37a178d50f58d6ca4
gonzoden <deniz.nazarova.99@gmail.com>
2021-09-23 12:47:17 +0600

    add ready project

---
+++ Pong/Assets/BallController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float MaxSpeed = 40;
+
+    public RigidBody rigidBody;
+
+    void Start()
+    {
+        rigidBody.GetComponent<RigidBody>();    
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidBody.velocity.magbitude > MaxSpeed)
+        {
+            rigidBody.velocity = rigidBody.velocity.normalized * MaxSpeed;
+        }
+    }
+}
